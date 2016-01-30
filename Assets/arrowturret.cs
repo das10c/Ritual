a761d2ec352de1f461f676489586ea9cf0f94656
das10c <das10c@gmail.com>
2016-01-30 15:58:52 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/testbackup 1.unity

---
+++ Assets/arrowturret.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrowturret : MonoBehaviour {
+    public GameObject arrowbolt;
+
+	// Update is called once per frame
+	void Update () {
+        //if(Input.GetButtonDown("Fire1"))
+        //{
+        GameObject arrow = Instantiate(arrowbolt, transform.position, Quaternion.identity) as GameObject;
+        arrow.GetComponent<Rigidbody>().AddForce(Vector3.up * 100f);
+        // }
+    }
+}
