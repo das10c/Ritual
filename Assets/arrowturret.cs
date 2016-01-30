e6849bc72aa1bfa302439d686d5db47c3b52a1af
mc12s <mc12s@my.fsu.edu>
2016-01-30 12:45:41 -0500

    arrowstuff

---
+++ Assets/arrowturret.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrowturret : MonoBehaviour {
+    public Rigidbody arrowbolts;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 100; i++)
+        {
+            var arrow = Instantiate(arrowbolts);
+        }
+    }
+}
