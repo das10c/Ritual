136b9eb975fa6a3a937dbdbb18408861edeb00ca
das10c <das10c@gmail.com>
2016-01-30 03:39:25 -0500

    Test Network
    Spawn players

---
+++ Assets/MyNetworkBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkBehavior : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    if (isLocalPlayer)
+        {
+            TopDownController tdc = GetComponent<TopDownController>();
+            if (tdc != null)
+            {
+                tdc.enabled = true;
+            }
+            UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController rbfpc = GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>();
+            if(rbfpc != null)
+            {
+                rbfpc.enabled = true;
+            }
+            GameObject camera = GameObject.Find("Scene Camera");
+            if (camera != null)
+            {
+                camera.SetActive(false);
+            }
+            GetComponentInChildren<Camera>().enabled = true;
+            GetComponentInChildren<AudioListener>().enabled = true;
+        }
+	}
+}
--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -1,19 +1,89 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.Networking;
-using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
 
-public class MyNetworkManager : NetworkManager {
+public class MyNetworkManager : NetworkManager
+{
+    // in the Network Manager component, you must put your player prefabs 
+    // in the Spawn Info -> Registered Spawnable Prefabs section 
+    public short playerPrefabIndex;
+    public NetworkStartPosition flyingStart, groundStart;
 
-    class MyManager : NetworkManager
+
+    public override void OnStartServer()
     {
-        public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
+        NetworkServer.RegisterHandler(MsgTypes.PlayerPrefab, OnResponsePrefab);
+        flyingStart.gameObject.SetActive(false);
+        groundStart.gameObject.SetActive(false);
+        print("here");
+        base.OnStartServer();
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        client.RegisterHandler(MsgTypes.PlayerPrefab, OnRequestPrefab);
+        base.OnClientConnect(conn);
+    }
+
+    private void OnRequestPrefab(NetworkMessage netMsg)
+    {
+        MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+        msg.controllerID = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>().controllerID;
+        msg.prefabIndex = playerPrefabIndex;
+        client.Send(MsgTypes.PlayerPrefab, msg);
+    }
+
+    private void OnResponsePrefab(NetworkMessage netMsg)
+    {
+        if (playerPrefabIndex == 0)
         {
-            if (extraMessageReader != null)
-            {
-                var s = extraMessageReader.ReadMessage<StringMessage>();
-                Debug.Log("my name is " + s.value);
-            }
-            OnServerAddPlayer(conn, playerControllerId, extraMessageReader);
+            flyingStart.gameObject.SetActive(true);
+            groundStart.gameObject.SetActive(false);
+
         }
+        else
+        {
+            flyingStart.gameObject.SetActive(false);
+            groundStart.gameObject.SetActive(true);
+        }
+        MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();
+        playerPrefab = spawnPrefabs[msg.prefabIndex];
+        base.OnServerAddPlayer(netMsg.conn, msg.controllerID);
+        Debug.Log(playerPrefab.name + " spawned!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+        msg.controllerID = playerControllerId;
+        NetworkServer.SendToClient(conn.connectionId, MsgTypes.PlayerPrefab, msg);
+    }
+
+    // I have put a toggle UI on gameObjects called PC1 and PC2 to select two different character types.
+    // on toggle, this function is called, which updates the playerPrefabIndex
+    // The index will be the number from the registered spawnable prefabs that 
+    // you want for your player
+    public void UpdatePC()
+    {
+        if (GameObject.Find("PC1").GetComponent<Toggle>().isOn)
+        {
+            playerPrefabIndex = 0;
+        }
+        else if (GameObject.Find("PC2").GetComponent<Toggle>().isOn)
+        {
+            playerPrefabIndex = 1   ;
+        }
+    }
+}
+
+public class MsgTypes
+{
+    public const short PlayerPrefab = MsgType.Highest + 1;
+
+    public class PlayerPrefabMsg : MessageBase
+    {
+        public short controllerID;
+        public short prefabIndex;
     }
 }
