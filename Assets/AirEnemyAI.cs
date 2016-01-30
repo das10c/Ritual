a2e633daa78c425f1f7636726f79d7d84c725981
das10c <das10c@gmail.com>
2016-01-30 12:41:45 -0500

    Started making enemys/fixed spawn

---
+++ Assets/AirEnemyAI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirEnemyAI : MonoBehaviour
+{
+    GameObject player = null;
+    Transform playerT = null;
+    float MoveSpeed = 4;
+    float MaxDist = 10;
+    float MinDist = 5;
+
+    void Update()
+    {
+        Chase();
+    }
+
+    void Chase()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Flying");
+        }
+        else if(playerT == null)
+        {
+            playerT = player.transform;
+        }
+        else
+        {
+            transform.LookAt(playerT);
+            if (Vector3.Distance(transform.position, playerT.position) >= MinDist)
+            {
+
+                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+            }
+        }
+
+    }
+}
--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -1,98 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
-using UnityEngine.UI;
 
 public class MyNetworkManager : NetworkManager
 {
-    // in the Network Manager component, you must put your player prefabs 
-    // in the Spawn Info -> Registered Spawnable Prefabs section 
-    static short playerPrefabIndex = 0;
-    bool first = true;
+    int spawnId = 0;
     GameObject spawner;
 
     public override void OnStartServer()
     {
+        base.OnStartServer();
+        spawnId = 0;
         spawner = GameObject.FindGameObjectWithTag("Respawn");
         if (spawner == null)
         {
-            Debug.LogError("No Spawn Placed");
+            Debug.LogError("Add Player Spawer to level");
         }
-        if (!first)
-        {
-            first = true;
-            playerPrefabIndex = 0;
-            Vector3 temp = spawner.transform.position;
-            temp.y -= 10;
-            spawner.transform.position = temp;
-        }
-        NetworkServer.RegisterHandler(MsgTypes.PlayerPrefab, OnResponsePrefab);
-        base.OnStartServer();
-    }
-
-    public override void OnClientConnect(NetworkConnection conn)
-    {
-        client.RegisterHandler(MsgTypes.PlayerPrefab, OnRequestPrefab);
-        base.OnClientConnect(conn);
-    }
-
-    private void OnRequestPrefab(NetworkMessage netMsg)
-    {
-        MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
-        msg.controllerID = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>().controllerID;
-        msg.prefabIndex = playerPrefabIndex;
-        client.Send(MsgTypes.PlayerPrefab, msg);
-    }
-
-    private void OnResponsePrefab(NetworkMessage netMsg)
-    {
-        MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();
-        playerPrefab = spawnPrefabs[msg.prefabIndex];
-        base.OnServerAddPlayer(netMsg.conn, msg.controllerID);
-        Debug.Log(playerPrefab.name + " spawned!");
-        Debug.LogError(playerPrefabIndex);
-        if (first)
-        {
-            first = false;
-            playerPrefabIndex = 1;
-            Vector3 temp = spawner.transform.position;
-            temp.y += 10;
-            spawner.transform.position = temp;
-        }
+        Vector3 temp = spawner.transform.position;
+        temp.y = 1.1f;
+        spawner.transform.position = temp;
     }
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
-        MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
-        msg.controllerID = playerControllerId;
-        NetworkServer.SendToClient(conn.connectionId, MsgTypes.PlayerPrefab, msg);
-
-    }
-
-    // I have put a toggle UI on gameObjects called PC1 and PC2 to select two different character types.
-    // on toggle, this function is called, which updates the playerPrefabIndex
-    // The index will be the number from the registered spawnable prefabs that 
-    // you want for your player
-    public void UpdatePC()
-    {
-        if (GameObject.Find("PC1").GetComponent<Toggle>().isOn)
+        playerPrefab = spawnPrefabs[spawnId];
+        base.OnServerAddPlayer(conn, playerControllerId);
+        if (spawnId == 0)
         {
-            playerPrefabIndex = 0;
-        }
-        else if (GameObject.Find("PC2").GetComponent<Toggle>().isOn)
-        {
-            playerPrefabIndex = 1   ;
+            Vector3 temp = spawner.transform.position;
+            temp.y = 20;
+            spawner.transform.position = temp;
+            spawnId = 1;
         }
     }
-}
 
-public class MsgTypes
-{
-    public const short PlayerPrefab = MsgType.Highest + 1;
 
-    public class PlayerPrefabMsg : MessageBase
-    {
-        public short controllerID;
-        public short prefabIndex;
-    }
 }
