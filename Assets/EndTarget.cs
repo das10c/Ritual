cd1216c1151061079b8ebebee0930ff23a8190f9
das10c <das10c@gmail.com>
2016-01-30 22:55:39 -0500

    Start to build a level

---
+++ Assets/EndTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTarget : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            MySceneManager.EndGame();
+        }
+    }
+}
--- Assets/GroundActions.cs
+++ Assets/GroundActions.cs
@@ -5,10 +5,11 @@ public class GroundActions : MonoBehaviour {
 
     public float meleeStrength = 35;
     public float meleeRange = 3;
-    public float strikeWidth = 1;
+    public GameObject marker;
+    float doubleClickStart = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
@@ -24,6 +25,18 @@ public class GroundActions : MonoBehaviour {
                 if (enemy != null)
                 {
                     enemy.health -= meleeStrength;
+                }
+            }
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+            {
+                GroundEnemyAI enemy = hit.collider.gameObject.GetComponent<GroundEnemyAI>();
+                if (enemy != null)
+                {
+                    GameObject m = Instantiate(marker, enemy.transform.position, Quaternion.identity) as GameObject;
+                    m.transform.parent = enemy.transform;
                 }
             }
         }
