a6ae9a05740daf2df0305ba66073a45990c41126
Hoekshot <hoekshot13@gmail.com>
2016-01-31 14:21:18 -0500

    Basic Health Pickup
    Pickup currently restores 25% of health.
    Added "PickupFactory" scene for easily viewing and modifying pickups.

---
+++ Assets/PickupHealthScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupHealthScript : MonoBehaviour {
+
+    GameObject player = null;
+    health h;
+
+    public float rotationRate = 50;
+    public float recoveryAmount = 25;
+
+	// Use this for initialization
+	void Start ()
+    {
+        h = FindObjectOfType<health>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(new Vector3 (0, rotationRate, 0) * Time.deltaTime);
+
+	    if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Ground");
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == player)
+        {
+            h.updateHealth(recoveryAmount);
+        }
+    }
+}
