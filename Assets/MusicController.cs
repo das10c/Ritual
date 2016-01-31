c02a1f917720e0f6d22277ee25131339317b5066
Hoekshot <hoekshot13@gmail.com>
2016-01-31 04:40:13 -0500

    Created Music Controller
    Background music should play automatically.
    Set up to fade a second track in and out if a second track is made
    later.

---
+++ Assets/MusicController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour {
+
+    public AudioMixerSnapshot outOfCombat;
+    public AudioMixerSnapshot inCombat;
+    public float transitionInMsec = 500;
+    public float transitionOutMsec = 2000;
+	
+    public void combatTransitionIn()
+    {
+        inCombat.TransitionTo(transitionInMsec);
+    }
+
+    public void combatTransitionOut()
+    {
+        inCombat.TransitionTo(transitionOutMsec);
+    }
+}
