ac0cd208b453675bbc9c564f3a1d99d9160d4211
das10c <das10c@gmail.com>
2016-01-31 15:44:13 -0500

    Added enemy spawn

---
+++ Assets/SpawnAirEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAirEnemy : MonoBehaviour {
+
+    public int timer = 0;
+    float passedTime = 0;
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(passedTime);
+        passedTime += Time.deltaTime;
+	    if (timer < passedTime)
+        {
+            passedTime = 0;
+            Instantiate(enemy, transform.position, Quaternion.identity);
+        }
+	}
+}
